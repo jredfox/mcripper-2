e0610d4de760b7abed8693e63d759170110cdbff
jredfox <jredfox@hotmail.com>
2021-01-08 01:45:09 -0600

    Update McChecker.java

--- src/jredfox/mcripper/utils/McChecker.java
+++ src/jredfox/mcripper/utils/McChecker.java
@@ -2,9 +2,7 @@ package jredfox.mcripper.utils;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.util.Collections;
 import java.util.List;
