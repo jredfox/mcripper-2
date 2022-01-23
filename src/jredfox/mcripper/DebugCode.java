1a085fcfb486827f6075b36e5df4f4e6a50242fb
jredfox <jredfox_@outlook.com>
2022-01-23 13:23:59 -0600

    Update DebugCode.java

--- src/jredfox/mcripper/DebugCode.java
+++ src/jredfox/mcripper/DebugCode.java
@@ -3,15 +3,24 @@ package jredfox.mcripper;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Map;
+import java.util.TreeMap;
 
-import jredfox.common.os.OSUtil;
-import jredfox.filededuper.util.DeDuperUtil;
+import com.jml.evilnotch.lib.JavaUtil;
+import com.jml.evilnotch.lib.json.JSONObject;
 
 public class DebugCode {
 	
 	public static void main(String[] args) throws MalformedURLException, IOException
 	{
-		System.out.println(DeDuperUtil.getDirFiles(new File(OSUtil.getAppData(), "Mcripper/mcripped/mojang/libraries")).toString().length());
+		JSONObject json = JavaUtil.getJSON(new File("C:/Users/jredfox/Desktop/index.json"));
+		TreeMap<Long, String> list = new TreeMap<>();
+		for(String hash : json.keySet())
+		{
+			list.put(Long.parseLong(hash), (String)json.get(hash));
+		}
+		for(Map.Entry<Long, String> map : list.entrySet())
+			System.out.println(map);
 	}
 
 }
